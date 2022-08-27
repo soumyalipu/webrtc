170dbb5f5000ae59bcc27258dfd82cc141337155
soumy <soumy@LAPTOP-50IJ58VD>
2022-08-27 15:22:08 +0530

    make it better

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const Socket = require("websocket").server;
 const http = require("http");
 
 const server = http.createServer((req, res) => {});
-
-server.listen(3000, () => {
+let port=process.env.PORT;
+if(port==null || port=""){
+  port=3000;
+}
+server.listen(port, () => {
   console.log("Listening on port 3000...");
 });
 
